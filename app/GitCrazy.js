5f3d24fe5badeefdd371fd4560ab2ee6f2a2e437
Brian Kelsey <brian.kelsey2@gmail.com>
2015-08-29 10:34:27 -0400

    Add event updates and redirects

--- app/GitCrazy.js
+++ app/GitCrazy.js
@@ -7,23 +7,24 @@ angular.module('HomeService', [])
     };
   }]);
 angular.module('EventController', [])
-  .controller('EventController', function($scope, $http) {
+  .controller('EventController', function($scope, $http, $location) {
 
     $scope.options = [
       {name: "Husband", id: 0},
       {name: "Family", id: 2},
       {name: "Wife", id: 3},
       {name: "Couple", id: 4}
-    ]
+    ];
     $scope.category = null;
     $scope.name = '';
 
     $scope.submit = function() {
       $http.get('/api/get').then(function(res) {
         var temp = res.data.doc;
+        console.log("Category: ", $scope.category);
         temp.category[$scope.category].size += 1;
         temp.category[$scope.category].events.push({'name': $scope.name, 'value': 1});
-        // console.log("Size: ", _.first(temp.category));
+        console.log(temp);
         $http.put('/api/update', temp)
           .success(function(data) {
             angular.forEach(data.category, function(val, key) {
@@ -34,6 +35,7 @@ angular.module('EventController', [])
               })
             })
             console.log('success');
+            $location.path('/home');
           })
           .error(function(data){
             console.log('error: ' + data);
